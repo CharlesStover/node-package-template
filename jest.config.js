7465e7a877acba38e304a54e1fb0585b31feddc8
Charles Stover <github@charlesstover.com>
2020-05-06 23:40:33 -0700

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,21 @@
 module.exports = {
+  collectCoverage: true,
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.{ts,tsx}',
+    '!<rootDir>/src/**/*.d.ts',
+    '!<rootDir>/src/**/*.test.{ts,tsx}',
+    '!<rootDir>/src/**/test-utils/*.{ts,tsx}',
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
   moduleNameMapper: {
-    '\\.s?css$': '<rootDir>/test-utils/empty.ts',
+    '\\.(?:css|gif|jpg|png|scss)$': '<rootDir>/test-utils/empty.ts',
   },
   roots: ['<rootDir>/src'],
   transform: {
