984c93d801e225692e6271dafa5028fb3dae0965
Charles Stover <github@charlesstover.com>
2020-06-14 10:25:11 -0700

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import commonjs from '@rollup/plugin-commonjs';
 import json from '@rollup/plugin-json';
 import nodeResolve from '@rollup/plugin-node-resolve';
 import url from '@rollup/plugin-url';
+import postcssUrl from 'postcss-url';
 import postcss from 'rollup-plugin-postcss';
 import typescript2 from 'rollup-plugin-typescript2';
 import packageJson from './package.json';
@@ -11,9 +12,15 @@ const EXTERNAL = new Set([
   ...Object.keys(packageJson.peerDependencies),
 ]);
 
+const IS_DEV = process.env.NODE_ENV === 'development';
+
+const MAIN_DIR = path.parse(packageJson.main).dir;
+
+const MODULE_DIR = path.parse(packageJson.module).dir;
+
 export default [
   {
-    input: 'src/index.ts',
+    cache: true,
     external(id) {
       if (EXTERNAL.has(id)) {
         return true;
@@ -27,36 +34,61 @@ export default [
 
       return false;
     },
+    indent: !IS_DEV,
+    input: 'src/index.ts',
     output: [
       {
-        file: packageJson.main,
+        dir: MAIN_DIR,
         format: 'cjs',
+        sourcemap: IS_DEV,
       },
       {
-        file: packageJson.module,
+        dir: MODULE_DIR,
         format: 'es',
+        sourcemap: IS_DEV,
       },
     ],
     plugins: [
       json({
-        compact: true,
+        compact: !IS_DEV,
       }),
       url(),
       postcss({
         autoModules: true,
         extract: false,
-        minimize: true,
+        minimize: !IS_DEV,
+        plugins: [
+          postcssUrl([
+            {
+              encodeType: 'base64',
+              maxSize: 1024,
+              url: 'inline',
+            },
+            {
+              assetsPath: 'images',
+              url: 'copy',
+              useHash: true,
+            },
+          ]),
+        ],
         use: ['sass'],
       }),
       nodeResolve({
         extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        preferBuiltins: true,
       }),
       commonjs({
         extensions: ['.js', '.jsx'],
       }),
       typescript2({
+        check: !IS_DEV,
         useTsconfigDeclarationDir: true,
       }),
     ],
+    sourcemaps: IS_DEV,
+    treeshake: !IS_DEV,
+    watch: {
+      exclude: 'node_modules/**',
+    },
   },
 ];
